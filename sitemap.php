7ae8bbb4c604b738e1f9051badab9d043ee9def4
LuisFAires <81849011+LuisFAires@users.noreply.github.com>
2021-07-26 02:31:10 -0300

    Add files via upload

--- sitemap.php
+++ sitemap.php
@@ -4,29 +4,28 @@
     $result = mysqli_query($connection, $sql);
     header("Content-Type: application/xml; charset=utf-8");
 
-    echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL; 
-    
-    echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' . PHP_EOL;
-    
+    echo '<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
+
     echo '<url>' . PHP_EOL;
-    echo '<loc>'.$_SERVER['HTTP_HOST'].'</loc>' . PHP_EOL;
+    echo '<loc>'."https://www.".$_SERVER['HTTP_HOST'].'</loc>' . PHP_EOL;
     echo '</url>' . PHP_EOL;
 
     echo '<url>' . PHP_EOL;
-    echo '<loc>'.$_SERVER['HTTP_HOST'].'/about.php</loc>' . PHP_EOL;
+    echo '<loc>'."https://www.".$_SERVER['HTTP_HOST'].'/about.php</loc>' . PHP_EOL;
     echo '</url>' . PHP_EOL;
 
     echo '<url>' . PHP_EOL;
-    echo '<loc>'.$_SERVER['HTTP_HOST'].'/contact.php</loc>' . PHP_EOL;
+    echo '<loc>'."https://www.".$_SERVER['HTTP_HOST'].'/contact.php</loc>' . PHP_EOL;
     echo '</url>' . PHP_EOL;
 
     echo '<url>' . PHP_EOL;
-    echo '<loc>'.$_SERVER['HTTP_HOST'].'/privacy.php</loc>' . PHP_EOL;
+    echo '<loc>'."https://www.".$_SERVER['HTTP_HOST'].'/privacy.php</loc>' . PHP_EOL;
     echo '</url>' . PHP_EOL;
 
     while($row = mysqli_fetch_array($result)){
         echo '<url>' . PHP_EOL;
-        echo '<loc>'.htmlspecialchars($_SERVER['HTTP_HOST'].'/mensagem.php?id='.$row['id'].'&titulo='.urlencode($row['titulo'])).'</loc>' . PHP_EOL;
+        echo '<loc>'.htmlspecialchars("https://www.".$_SERVER['HTTP_HOST'].'/mensagem.php?id='.$row['id'].'&titulo='.urlencode($row['titulo'])).'</loc>' . PHP_EOL;
         echo '</url>' . PHP_EOL;
     }
     
