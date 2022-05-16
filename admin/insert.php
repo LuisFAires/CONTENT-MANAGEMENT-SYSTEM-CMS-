38109e283b9c3d23168607529bbb77e5d31c6860
Luis Fillipe Aires Souza <81849011+LuisFAires@users.noreply.github.com>
2022-05-16 15:06:51 -0300

    Correção no cadastro

--- admin/insert.php
+++ admin/insert.php
@@ -1,10 +1,12 @@
+<?php
+    require_once "checkLogin.php";
+?>
 <head>
     <link rel="stylesheet" type="text/css" href="../style.css">
 </head>
 <?php
-    require_once "checkLogin.php";
     require_once "connection.php";
-    require_once "admFunctions.php";    
+    require_once "admFunctions.php";
     if(isset($_POST['titulo'], $_POST['texto'], $_FILES['img'], $_FILES['imgbg'], $_POST['cor1'], $_POST['cor2']) && $_POST['form_name'] === "Cadastrar"){
         $img = fileUpload($_FILES['img'], "../img/");
         if($img != false){
